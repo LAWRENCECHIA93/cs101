5e7f535fe4fdec0e2c20596fddd307e731f88595
B1229016 <93651509+LAWRENCECHIA93@users.noreply.github.com>
2023-12-14 15:35:23 +0800

    Update main5.c

--- EX2/main5.c
+++ EX2/main5.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+   int i = 130;
+   if (i<=30){
+       printf("免費");
+   }
+   else if (i>=240){
+       printf("240元");
+   }
+   else{
+       printf("%d 元", i);
+   }
+    return 0;
+}
 

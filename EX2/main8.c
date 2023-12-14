9111d44dd6c86d5880dd730d1fe61f57230a0e71
B1229016 <93651509+LAWRENCECHIA93@users.noreply.github.com>
2023-12-14 15:52:14 +0800

    Update main8.c

--- EX2/main8.c
+++ EX2/main8.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+   int x = -8;
+   int y = 7;
+   int z = 5;
+   if (x<0){
+       int sum = (x*-1)*100 + y*10 + z;
+       printf("%d",sum*-1);
+   }
+   else {
+       printf("%d", x*100 + y*10 + z);
+   }
+   return 0;
+}
 

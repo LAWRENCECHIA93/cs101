cc04f07d5044d2570e025252fe0b5c7cff6d2b52
B1229016 <93651509+LAWRENCECHIA93@users.noreply.github.com>
2023-12-21 00:00:20 +0800

    Update main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+    double pi = 0.0f;
+    int j = 1;
+    for (int n = 0; n < 1000000; n++) {
+        pi += j * 4.0 / (2 * n + 1);
+        j = -j;
+    }
+    printf("%.5f\n", pi);
+
+    return 0;
+}

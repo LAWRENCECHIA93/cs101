37376da5210229d8cb7fe83123e6f1d4e8a398d9
B1229016 <93651509+LAWRENCECHIA93@users.noreply.github.com>
2023-12-20 23:09:29 +0800

    Update main3.c

--- hw5/main3.c
+++ hw5/main3.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+
+    for (int i = 1; i <= 9 * 9; i++) {
+        printf("%d * %d = %d  ", (i - 1) / 9 + 1, (i - 1) % 9 + 1, ((i - 1) / 9 + 1) * ((i - 1) % 9 + 1));
+        
+        if (i % 9 == 0) {
+            printf("\n");
+        }
+    }
+
+    return 0;
+}

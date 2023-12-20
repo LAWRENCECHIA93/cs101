82a289e33b09809c600463905e4ea271ed2067fb
B1229016 <93651509+LAWRENCECHIA93@users.noreply.github.com>
2023-12-20 22:32:48 +0800

    Update main1.c

--- hw5/main1.c
+++ hw5/main1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 7;
+    for(int i = 1;i<=7;i++){
+        for(int j = 1;j<n;j++){
+            printf(" ");
+        }
+        for(int k = 1;k<=i;k++){
+            printf("%d ",i);
+        }
+        printf("\n");
+        n--;
+    }
+    return 0;
+}
 

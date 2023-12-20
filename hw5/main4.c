19ab474c90fd260b6295a0926154f3888a6fa26e
B1229016 <93651509+LAWRENCECHIA93@users.noreply.github.com>
2023-12-20 23:52:05 +0800

    Update main4.c

--- hw5/main4.c
+++ hw5/main4.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int i = 12345;
+    int thou = (i / 1000) % 10;
+    int unit = i % 10;
+    int swapped_i = i + unit * 1000 - thou * 1000 - unit + thou;
+    printf("%d\n", i);
+    printf("%d\n", swapped_i);
+    return 0;
+}
 

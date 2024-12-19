7aa068fa25c8327f755addd791845b8a5ff552c0
B1229016 <93651509+LAWRENCECHIA93@users.noreply.github.com>
2024-12-19 10:40:33 +0800

    Create main2.c

---
+++ EX3/main2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 15;
+    
+    if (i % 15 == 0){
+        printf("Love IU");
+    }
+    else if (i % 3 == 0){
+        printf("Love");
+    }
+    else if (i % 5 == 0){
+        printf("IU");
+    }
+    else{
+        printf("%d",i);
+    }
+    return 0;
+}
